44d98116ed3990a5c82617aa13bb926a76da1b7c
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-07-30 23:56:09 +0200

    Add files via upload

--- minigame.js
+++ minigame.js
@@ -11,4 +11,14 @@ window.onload = () => {
     }
     window.oncontextmenu = e => e.preventDefault()
     b.oncontextmenu = e => e.preventDefault()
+    const laser = () => {
+        let c = document.createElement("div")
+        document.body.appendChild(c)
+        let d = parseFloat(b.style.left) + 15
+        if (b.style.left == "calc(50% - 25px)") c.style = "position:fixed;bottom:44px;left:calc(50% - 10px);background-color:red;width:20px;height:60px;z-index:-2"
+        else c.style = `position:fixed;bottom:44px;left:${d}px;background-color:red;width:20px;height:60px;z-index:-2`
+        const move = () => c.style.bottom = (parseFloat(c.style.bottom) + 6) + "px"
+        setInterval(move, 1)
+    }
+    setInterval(laser, 600)
 }
