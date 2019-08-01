b23e878132fc662ef122caec29e9637ef4bd36a0
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-08-01 16:03:20 +0200

    Add files via upload

--- minigame.js
+++ minigame.js
@@ -2,8 +2,7 @@ window.onload = () => {
     let b = document.getElementById("ship")
     window.onmousemove = e => b.style.left = (e.pageX - 25) + "px"
     window.ontouchmove = e => {
-        if (b.style.transition == "left 0.07s linear 0s") b.style.transition = ""
-        else if (b.style.transition == "opacity 1s ease 0s, left 0.07s linear 0s" || b.style.transition == "left 0.07s linear 0s, opacity 1s ease 0s") b.style.transition = "opacity 1s ease 0s"
+        b.style.transition = ""
         b.style.left = (e.pageX - 25) + "px"
     }
     window.ontouchstart = e => {
