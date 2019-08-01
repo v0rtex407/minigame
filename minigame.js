e78d32ccc94c9573088acd6f24df522a36be325e
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-08-01 10:58:25 +0200

    Add files via upload

--- minigame.js
+++ minigame.js
@@ -6,6 +6,7 @@ window.onload = () => {
         b.style.left = (e.pageX - 25) + "px"
     }
     window.ontouchstart = e => {
+        e.preventDefault()
         b.style.left = (e.pageX - 25) + "px"
         b.style.transition = "left 0.07s linear"
     }
@@ -25,64 +26,37 @@ window.onload = () => {
         track.push(y)
         const fall = () => {
             c.style.top = (parseFloat(c.style.top) + 1.2) + "px";
-            if ((b.offsetTop <= c.offsetTop + 70 && b.offsetTop + 90 >= c.offsetTop) && (b.offsetLeft >= c.offsetLeft - 50 && b.offsetLeft <= c.offsetLeft + 70)) {
-                if (b.firstChild.getAttribute("src") == "3lives.png" && b.style.transition != "opacity 1s ease 0s") {
-                    b.firstChild.src = "2lives.png";
-                    c.setAttribute("value", "1")
-                    b.style.transition = "opacity 1s ease 0s"
-                    b.style.opacity = "0.5"
-                    const glow = () => {
-                        b.style.opacity = "1"
-                        b.removeEventListener("transitionend", glow)
-                        const glow2 = () => {
-                            b.style.opacity = "0.5"
-                            b.removeEventListener("transitionend", glow2)
-                            const glow3 = () => {
-                                b.style.opacity = "1"
-                                b.removeEventListener("transitionend", glow3)
-                                const glow4 = () => {
-                                    b.style.transition = ""
-                                    c.value = "0"
-                                    b.removeEventListener("transitionend", glow4)
-                                }
-                                b.addEventListener("transitionend", glow4)
-                            }
-                            b.addEventListener("transitionend", glow3)
-                        }
-                        b.addEventListener("transitionend", glow2)
-                    }
-                    b.addEventListener("transitionend", glow)
-                } else if (b.firstChild.getAttribute("src") == "2lives.png" && c.getAttribute("value") != "1" && b.style.transition != "opacity 1s ease 0s") {
-                    b.firstChild.src = "1life.png"
-                    c.setAttribute("value", "1")
-                    b.style.transition = "opacity 1s ease 0s"
-                    b.style.opacity = "0.5"
-                    const glow = () => {
-                        b.style.opacity = "1"
-                        b.removeEventListener("transitionend", glow)
-                        const glow2 = () => {
-                            b.style.opacity = "0.5"
-                            b.removeEventListener("transitionend", glow2)
-                            const glow3 = () => {
-                                b.style.opacity = "1"
-                                b.removeEventListener("transitionend", glow3)
-                                const glow4 = () => {
-                                    b.style.transition = ""
-                                    c.value = "0"
-                                    b.removeEventListener("transitionend", glow4)
-                                }
-                                b.addEventListener("transitionend", glow4)
-                            }
-                            b.addEventListener("transitionend", glow3)
-                        }
-                        b.addEventListener("transitionend", glow2)
-                    }
-                    b.addEventListener("transitionend", glow)
-                } else if (b.firstChild.getAttribute("src") == "1life.png" && c.getAttribute("value") != "1" &&
-                    b.style.transition != "opacity 1s ease 0s") {
+            if ((b.offsetTop <= c.offsetTop + 70 && b.offsetTop + 90 >= c.offsetTop) && (b.offsetLeft >= c.offsetLeft - 50 && b.offsetLeft <= c.offsetLeft + 70) && c.getAttribute("value") != "1" && b.style.transition != "opacity 1s ease 0s") {
+                if (b.firstChild.getAttribute("src") == "3lives.png") b.firstChild.src = "2lives.png";
+                else if (b.firstChild.getAttribute("src") == "2lives.png") b.firstChild.src = "1life.png"
+                else if (b.firstChild.getAttribute("src") == "1life.png") {
                     alert("Don't give up, you can do it!")
                     window.location.reload()
                 }
+                c.setAttribute("value", "1")
+                b.style.transition = "opacity 1s ease 0s"
+                b.style.opacity = "0.5"
+                const glow = () => {
+                    b.style.opacity = "1"
+                    b.removeEventListener("transitionend", glow)
+                    const glow2 = () => {
+                        b.style.opacity = "0.5"
+                        b.removeEventListener("transitionend", glow2)
+                        const glow3 = () => {
+                            b.style.opacity = "1"
+                            b.removeEventListener("transitionend", glow3)
+                            const glow4 = () => {
+                                b.style.transition = ""
+                                c.value = "0"
+                                b.removeEventListener("transitionend", glow4)
+                            }
+                            b.addEventListener("transitionend", glow4)
+                        }
+                        b.addEventListener("transitionend", glow3)
+                    }
+                    b.addEventListener("transitionend", glow2)
+                }
+                b.addEventListener("transitionend", glow)
             }
         }
         setInterval(fall, 1)
