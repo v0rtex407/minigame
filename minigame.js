c2df15511f8fa40e2e515c169212b4ea931f3c16
v0rtex407 <48419859+v0rtex407@users.noreply.github.com>
2019-08-04 16:46:06 +0200

    Add files via upload

--- minigame.js
+++ minigame.js
@@ -102,13 +102,10 @@ window.onload = () => {
     }
     setInterval(laser, 600)
     let visibility = undefined
-    if (window.matchMedia("max-device-width:1000px").matches) window.onblur = () => alert("The blocks are getting stronger, hurry up!")
-    else {
-        document.onvisibilitychange = () => {
-            if (visibility == undefined) {
-                visibility = 1
-                visibility = alert("The blocks are getting stronger, hurry up!")
-            }
+    if (!window.matchMedia("max-device-width:1000px").matches) document.onvisibilitychange = () => {
+        if (visibility == undefined) {
+            visibility = 1
+            visibility = alert("The blocks are getting stronger, hurry up!")
         }
     }
 }
